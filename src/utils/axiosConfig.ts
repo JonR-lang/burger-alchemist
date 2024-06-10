697fadd425ca0ca5fda32fd08e246a6a95334f02
JohnnyScript <iroelejohnny@gmail.com>
2024-06-10 11:49:02 +0100

    Removed the unused localhost variable from the axiosConfig

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -10,7 +10,6 @@ type User = {
 };
 
 const BASE_URL = import.meta.env.VITE_API_URL;
-const localhost = "http://localhost:5000/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
